00efcc308197566bb74a8894c05f720e53c730ca
Nicolas <nick2003@email.cz>
2024-03-04 23:54:10 +0100

    pridani zod

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -7,25 +7,35 @@ import Link from "next/link";
 import GoogleLoginBtn from "@/components/GoogleLoginBtn";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const Login = () => {
   const router = useRouter();
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
   const [showPassword, setShowPassword] = useState(false);
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       const res = await signIn("credentials", {
-        email,
-        password,
+        email: values.email,
+        password: values.password,
         redirect: false,
       });
 
       if (res?.error) {
         return;
       } else {
+        form.reset();
         router.replace("/user");
       }
     } catch (err) {
@@ -33,58 +43,93 @@ const Login = () => {
     }
   };
 
+  const formSchema = z.object({
+    email: z.string().email({ message: "Neplatný email" }),
+    password: z.string().min(8, { message: "Heslo musí mít alespoň 8 písmen" }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <section className="flex justify-center items-center flex-col  h-dvh w-screen ">
       <main className=" text-center py-8 px-10 flex flex-col relative gap-3 rounded-lg ">
         <h2 className="mb-5  font-bold sm-clamp ">Vítejte zpět! 👋</h2>
-        <form className="w-full flex flex-col gap-5" onSubmit={handleSubmit}>
-          <div className="flex flex-col w-full">
-            <label className="text-start">Email</label>
-            <div className="w-full relative">
-              <input
-                onChange={(e) => setEmail(e.target.value)}
-                autoFocus
-                name="email"
-                type="email"
-                placeholder="Zadejte svůj email..."
-                className="outline-2 outline-transparent shadow-lg px-5 py-2 rounded-lg pl-10 w-full focus-within:outline-primary focus-within:outline-2 transition-all duration-300 input"
-              />
-              <IoMdMail className="absolute top-1/2 -translate-y-1/2 left-2" />
-            </div>
-          </div>
-          <div className="flex flex-col w-full">
-            <label className="text-start">Heslo</label>
-            <div className="w-full relative">
-              <input
-                onChange={(e) => setPassword(e.target.value)}
-                id="password"
-                name="password"
-                type={showPassword ? "text" : "password"}
-                placeholder="Zadejte své heslo"
-                className="outline-2 shadow-lg px-5 py-2 rounded-lg pl-10 w-full focus-within:outline-primary focus-within:outline-2 transition-all duration-300 input"
-              />
-              <FaLock className="absolute top-1/2 -translate-y-1/2 left-2" />
-              {showPassword ? (
-                <FaEyeSlash
-                  onClick={() => setShowPassword(false)}
-                  className="absolute top-1/2 -translate-y-1/2 right-2 cursor-pointer"
-                />
-              ) : (
-                <FaEye
-                  onClick={() => setShowPassword(true)}
-                  className="absolute top-1/2 -translate-y-1/2 right-2 cursor-pointer"
-                />
-              )}
-            </div>
-          </div>
-
-          <button
-            type="submit"
-            className="p-3 bg-primary text-white border-none outline-none rounded-lg mt-3 cursor-pointer transition-all hover:scale-105 hover:brightness-105 active:scale-95 active:brightness-95"
+        <Form {...form}>
+          <form
+            className="w-full flex flex-col gap-5"
+            onSubmit={form.handleSubmit(onSubmit)}
           >
-            Přihlásit se
-          </button>
-        </form>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <div className="relative w-full">
+                      <Input
+                        autoFocus
+                        type="email"
+                        placeholder="Zadejte svůj email..."
+                        className="outline-2 outline-transparent shadow-lg  py-2 rounded-lg px-5 pl-10 w-full focus-within:outline-primary focus-within:outline-2 transition-all duration-300 input"
+                        {...field}
+                      />
+                      <IoMdMail className="absolute top-1/2 -translate-y-1/2 left-2" />
+                    </div>
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Heslo</FormLabel>
+                  <FormControl>
+                    <div className="relative w-full">
+                      <Input
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Zadejte své heslo"
+                        className="outline-2 shadow-lg px-5 py-2 rounded-lg pl-10 w-full focus-within:outline-primary focus-within:outline-2 transition-all duration-300 input"
+                        {...field}
+                      />
+                      <FaLock className="absolute top-1/2 -translate-y-1/2 left-2" />
+                      {showPassword ? (
+                        <FaEyeSlash
+                          onClick={() => setShowPassword(false)}
+                          className="absolute top-1/2 -translate-y-1/2 right-2 cursor-pointer"
+                        />
+                      ) : (
+                        <FaEye
+                          onClick={() => setShowPassword(true)}
+                          className="absolute top-1/2 -translate-y-1/2 right-2 cursor-pointer"
+                        />
+                      )}
+                    </div>
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <button
+              type="submit"
+              className="p-3 bg-primary text-white border-none outline-none rounded-lg mt-3 cursor-pointer transition-all hover:scale-105 hover:brightness-105 active:scale-95 active:brightness-95"
+            >
+              Přihlásit se
+            </button>
+          </form>
+        </Form>
         <div className="w-full h-[2px] #bfc3cc] relative mt-5">
           <span className="absolute left-1/2 -top-3 -translate-x-1/2 bg-white px-3">
             NEBO
--- app/(pages)/register/page.tsx
+++ app/(pages)/register/page.tsx
@@ -1,36 +1,42 @@
 "use client";
 
-import React, { useState, FormEvent } from "react";
+import React, { useState } from "react";
 import { FaLock, FaEye, FaUser, FaEyeSlash } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { toast } from "sonner";
-
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 //@ts-ignore
 import { CldUploadWidget } from "next-cloudinary";
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const Register = () => {
   const router = useRouter();
-  const [name, setName] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [profilePictureUrl, setProfilePictureUrl] = useState<string>("");
+  const [profilePicture, setProfilePicture] = useState<string>("");
   const [preview, setPreview] = useState<string>("");
   const [showPassword, setShowPassword] = useState(false);
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       const resUserExists = await fetch("/api/userExists", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email }),
+        body: JSON.stringify({ email: values.email }),
       });
 
       const { user } = await resUserExists.json();
@@ -54,26 +60,45 @@ const Register = () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          name,
-          email,
-          password,
-          profilePicture: profilePictureUrl,
+          name: values.name,
+          email: values.email,
+          password: values.password,
+          profilePicture: profilePicture,
         }),
       });
 
       if (res.ok) {
-        const form = e.target as HTMLFormElement;
         form.reset();
         router.push("/login");
         toast.success("Uživatel zaregistrován");
       }
     } catch (err) {
-      console.log("error occurred during registration ", err);
+      console.log(err);
       toast.error("Nepodařilo se zaregistrovat");
     }
   };
 
   const MemoizedAvatarImage = React.memo(AvatarImage);
+
+  const formSchema = z.object({
+    name: z
+      .string()
+      .min(2, { message: "Jméno musí mít alespoň 2 písmena" })
+      .max(50, { message: "Jméno nesmí být delší než 50 písmen" }),
+    email: z.string().email({ message: "Neplatný email" }),
+    password: z.string().min(8, { message: "Heslo musí mít alespoň 8 písmen" }),
+    profilePicture: z.any(),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      profilePicture: "",
+    },
+  });
 
   return (
     <section className="flex justify-center items-center flex-col  w-screen mt-20  ">
@@ -83,117 +108,162 @@ const Register = () => {
           A připoj se k největší komunitě{" "}
           <span className="font-bold text-primary">domácích kuchařů</span>!
         </p>
-        <form className="w-full flex flex-col gap-5" onSubmit={handleSubmit}>
-          <div className="flex flex-col w-full">
-            <label className="text-start">Uživatelské jméno</label>
-            <div className="w-full relative">
-              <input
-                onChange={(e) => setName(e.target.value)}
-                autoFocus
-                name="name"
-                type="text"
-                placeholder="Zadejte své jméno..."
-                className="outline-2 outline-transparent shadow-lg  py-2 rounded-lg px-5 pl-10 w-full focus-within:outline-primary focus-within:outline-2 transition-all duration-300 input"
-              />
-              <FaUser className="absolute top-1/2 -translate-y-1/2 left-2" />
-            </div>
-          </div>
-          <div className="flex flex-col w-full">
-            <label className="text-start">Email</label>
-            <div className="w-full relative">
-              <input
-                onChange={(e) => setEmail(e.target.value)}
-                name="email"
-                type="email"
-                placeholder="Zadejte svůj email..."
-                className="outline-2 outline-transparent shadow-lg px-5 py-2 rounded-lg pl-10 w-full focus-within:outline-primary focus-within:outline-2 transition-all duration-300 input"
-              />
-              <IoMdMail className="absolute top-1/2 -translate-y-1/2 left-2" />
-            </div>
-          </div>
-          <div className="flex flex-col w-full">
-            <label className="text-start">Heslo</label>
-            <div className="w-full relative">
-              <input
-                onChange={(e) => setPassword(e.target.value)}
-                id="password"
-                name="password"
-                type={showPassword ? "text" : "password"}
-                placeholder="Zadejte své heslo"
-                className="outline-2 shadow-lg px-5 py-2 rounded-lg pl-10 w-full focus-within:outline-primary focus-within:outline-2 transition-all duration-300 input"
-              />
-              <FaLock className="absolute top-1/2 -translate-y-1/2 left-2" />
-              {showPassword ? (
-                <FaEyeSlash
-                  onClick={() => setShowPassword(false)}
-                  className="absolute top-1/2 -translate-y-1/2 right-2 cursor-pointer"
-                />
-              ) : (
-                <FaEye
-                  onClick={() => setShowPassword(true)}
-                  className="absolute top-1/2 -translate-y-1/2 right-2 cursor-pointer"
-                />
-              )}
-            </div>
-
-            {preview ? (
-              <>
-                <div className="mt-5 rounded-lg flex justify-center items-center flex-col">
-                  <h3 className="text-lg mb-3">Váš profilový obrázek</h3>
-                  <Avatar className="h-[150px] w-[150px]">
-                    <MemoizedAvatarImage
-                      className="rounded-lg object-cover "
-                      src={preview}
-                    />
-                  </Avatar>
-                </div>
-              </>
-            ) : (
-              <CldUploadWidget
-                uploadPreset="pmhofx0x"
-                //@ts-ignore
-                onSuccess={(result, { widget }) => {
-                  //@ts-ignore
-                  setProfilePictureUrl(result?.info?.url);
-                  //@ts-ignore
-                  setPreview(result?.info?.url);
-                  //@ts-ignore
-                  widget.close();
-                  toast.success("Profilový obrázek byl nahrán");
-                }}
-                //@ts-ignore
-                onError={toast.error("Nepovedlo se nahrát profilový obrázek")}
-              >
-                {({ open }) => {
-                  function handleOnClick() {
-                    open();
-                  }
-                  return (
-                    <Button
-                      type="button"
-                      variant="outline"
-                      className=" mt-5 outline-primary outline"
-                      onClick={handleOnClick}
-                    >
-                      Nahrát profilový obrázek
-                    </Button>
-                  );
-                }}
-              </CldUploadWidget>
-            )}
-          </div>
-
-          <button
-            type="submit"
-            className="p-3 bg-primary text-white border-none outline-none rounded-lg mt-3 cursor-pointer transition-all hover:scale-105 hover:brightness-105 active:scale-95 active:brightness-95"
+        <Form {...form}>
+          <form
+            className="w-full flex flex-col gap-5"
+            onSubmit={form.handleSubmit(onSubmit)}
           >
-            Zaregistrovat se
-          </button>
-          <span>Už mezi nás patříte?</span>
-          <Link className=" text-primary" href="/login">
-            Přihlašte se
-          </Link>
-        </form>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Uživatelské jméno</FormLabel>
+                  <FormControl>
+                    <div className="relative w-full">
+                      <Input
+                        autoFocus
+                        type="text"
+                        placeholder="Zadejte své jméno..."
+                        className="outline-2 outline-transparent shadow-lg  py-2 rounded-lg px-5 pl-10 w-full focus-within:outline-primary focus-within:outline-2 transition-all duration-300 input"
+                        {...field}
+                      />
+                      <FaUser className="absolute top-1/2 -translate-y-1/2 left-2" />
+                    </div>
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <div className="relative w-full">
+                      <Input
+                        type="email"
+                        placeholder="Zadejte svůj email..."
+                        className="outline-2 outline-transparent shadow-lg px-5 py-2 rounded-lg pl-10 w-full focus-within:outline-primary focus-within:outline-2 transition-all duration-300 input"
+                        {...field}
+                      />
+                      <IoMdMail className="absolute top-1/2 -translate-y-1/2 left-2" />
+                    </div>
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Heslo</FormLabel>
+                  <FormControl>
+                    <div className="relative w-full">
+                      <Input
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Zadejte své heslo"
+                        className="outline-2 shadow-lg px-5 py-2 rounded-lg pl-10 w-full focus-within:outline-primary focus-within:outline-2 transition-all duration-300 input"
+                        {...field}
+                      />
+                      <FaLock className="absolute top-1/2 -translate-y-1/2 left-2" />
+                      {showPassword ? (
+                        <FaEyeSlash
+                          onClick={() => setShowPassword(false)}
+                          className="absolute top-1/2 -translate-y-1/2 right-2 cursor-pointer"
+                        />
+                      ) : (
+                        <FaEye
+                          onClick={() => setShowPassword(true)}
+                          className="absolute top-1/2 -translate-y-1/2 right-2 cursor-pointer"
+                        />
+                      )}
+                    </div>
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="profilePicture"
+              render={({ field }) => (
+                <FormItem>
+                  <div>
+                    {preview ? (
+                      <>
+                        <div className="mt-5 rounded-lg flex justify-center items-center flex-col">
+                          <h3 className="text-lg mb-3">
+                            Váš profilový obrázek
+                          </h3>
+                          <Avatar className="h-[150px] w-[150px]">
+                            <MemoizedAvatarImage
+                              className="rounded-lg object-cover "
+                              src={preview}
+                            />
+                          </Avatar>
+                        </div>
+                      </>
+                    ) : (
+                      <FormControl>
+                        <CldUploadWidget
+                          uploadPreset="pmhofx0x"
+                          //@ts-ignore
+                          onSuccess={(result, { widget }) => {
+                            //@ts-ignore
+                            setProfilePicture(result?.info?.url);
+                            //@ts-ignore
+                            setPreview(result?.info?.url);
+                            //@ts-ignore
+                            widget.close();
+                            toast.success("Profilový obrázek byl nahrán");
+                          }}
+                        >
+                          {({ open }) => {
+                            function handleOnClick() {
+                              open();
+                            }
+                            return (
+                              <Button
+                                type="button"
+                                variant="outline"
+                                className=" mt-5 outline-primary outline"
+                                onClick={handleOnClick}
+                              >
+                                Nahrát profilový obrázek
+                              </Button>
+                            );
+                          }}
+                        </CldUploadWidget>
+                      </FormControl>
+                    )}
+                  </div>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button
+              type="submit"
+              className="p-3 bg-primary text-white border-none outline-none rounded-lg mt-3 cursor-pointer transition-all hover:scale-105 hover:brightness-105 active:scale-95 active:brightness-95"
+            >
+              Zaregistrovat se
+            </Button>
+            <span>Už mezi nás patříte?</span>
+            <Link className=" text-primary" href="/login">
+              Přihlašte se
+            </Link>
+          </form>
+        </Form>
       </main>
     </section>
   );
